9f2a5f402ce9b511f9e1343bfc7fbdb2e888fc87
erikvsuris <erikvsuris@gmail.com>
2023-08-20 00:38:45 -0300

    Pokemons into List Items

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -3,7 +3,33 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function pokemonToListItem(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    
+                </ol>
+
+                <img src=""
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then(response => response.json())
-    .then(responseBody => console.log(responseBody))
+    .then(jsonBody => jsonBody.results)
+    .then(pokemons => {
+        for (let i of pokemons) {
+            const pokemon = i
+            pokemonList.innerHTML += pokemonToListItem(pokemon)
+        }
+    })
     .catch(error => console.log(error))
