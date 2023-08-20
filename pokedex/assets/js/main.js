ccc9cf78cd5bedc6a1b6837f33cf88843f16d2f4
erikvsuris <erikvsuris@gmail.com>
2023-08-20 16:39:14 -0300

    Pokedex finalizada

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,35 +1,47 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const maxRecords = 151
+const limit = 9
+let offset = 0
 
 function pokemonToListItem(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">${pokemon.number}</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.id}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    
+                    ${pokemon.types.map(type => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src=""
-                    alt="${pokemon.name}">
+                <img src="${pokemon.image}"
+                     alt="${pokemon.name}">
             </div>
         </li>
     `
 }
 
-const pokemonList = document.getElementById('pokemonList')
-
-fetch(url)
-    .then(response => response.json())
-    .then(jsonBody => jsonBody.results)
-    .then(pokemons => {
-        for (let i of pokemons) {
-            const pokemon = i
-            pokemonList.innerHTML += pokemonToListItem(pokemon)
-        }
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(pokemonToListItem).join('')
+        pokemonList.innerHTML += newHtml
     })
-    .catch(error => console.log(error))
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,34 @@
+const pokeApi = {}
+
+function pokemonDetailToPokemon(pokemonDetail) {
+    const pokemon = new Pokemon()
+    pokemon.id = pokemonDetail.id
+    pokemon.name = pokemonDetail.name
+
+    const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.image = pokemonDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then(response => response.json())
+        .then(pokemonDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset,limit) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
+}
